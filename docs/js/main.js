468683e1ce47dd0d3912c228b490879c13ae7d8b
flickerstop <maclean.joey@gmail.com>
2020-04-16 13:45:57 -0400

    Initial commit

---
+++ docs/js/main.js
@@ -0,0 +1,42 @@
+let sheets = null;
+
+function init(){
+    $.getJSON("./sheets.json", function(result){
+        sheets = result;
+
+        // set the title
+        d3.select("#currentTitle").html(sheets.currentSheetTitle);
+
+        // set the current sheet
+        d3.select("#currentSpreadsheet").attr("src",sheets.currentSheetURL)
+            .attr("width",parseInt($(window).width())-50)
+            .attr("height",parseInt($(window).height())-200);
+
+
+        // create the dropdown list
+        let select = d3.select("#dropDownListDiv").append("select").attr("id","dropDownList").on("change",changeSheet).attr("class","select-css");
+
+        addOption(select,sheets.currentSheetTitle,{url:sheets.currentSheetURL,title:sheets.currentSheetTitle});
+
+        for(let option of sheets.otherSheets){
+            addOption(select,option.title,option);
+        }
+    });
+
+
+    function addOption(selectList,title,value){
+        selectList.append("option").attr("value",JSON.stringify(value)).html(title);
+    }
+}
+
+function changeSheet(){
+    let value = JSON.parse($("#dropDownList :selected").val());
+
+    // Change the title
+    d3.select("#currentTitle").html(value.title);
+
+    // Change the url
+    d3.select("#currentSpreadsheet").attr("src",value.url)
+            .attr("width",parseInt($(window).width())-50)
+            .attr("height",parseInt($(window).height())-200);
+}
