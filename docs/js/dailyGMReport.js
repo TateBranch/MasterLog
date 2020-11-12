cd5fc41ea2c1d326d4bf75bb8177783baecb33bf
unknown <chris@tatebranch.com>
2020-11-12 11:17:42 -0700

    test #3
    deleted previous changes, copied f&I javascript/json/html because we want to utlizie the 3 tabs at the bottom like the F&I section has

--- docs/js/dailyGMReport.js
+++ docs/js/dailyGMReport.js
@@ -1,26 +1,60 @@
 let sheets = null;
 
 function init(){
-    $.getJSON("./json/bestEver.json", function(result){
-        sheets = result;
+    $.getJSON("./json/dailyGMReport.json", function(sheets){
 
         // set the title
-        d3.select("#currentTitle").html(sheets.currentSheetTitle);
+        d3.select("#currentTitle").html("Daily GM Report");
 
         // set the current sheet
         d3.select("#currentSpreadsheet").attr("src",sheets.currentSheetURL)
             .attr("width",parseInt($(window).width())-2)
             .attr("height",parseInt($(window).height())-28);
+		
+		d3.select("#errorText").style("top",(parseInt($(window).height())-185)/2);
+		d3.select("#errorText").style("left",(parseInt($(window).width())-550)/2);
 
-
-        // create the dropdown list
-        let select = d3.select("#dropDownListDiv").append("select").attr("id","dropDownList").on("change",changeSheet).attr("class","select-css");
-
-        addOption(select,sheets.currentSheetTitle,{url:sheets.currentSheetURL,title:sheets.currentSheetTitle});
-
-        for(let option of sheets.otherSheets){
-            addOption(select,option.title,option);
-        }
+		
+		////////////////////////////////////////////
+		// Artesia
+		let artesiaDropDown = d3.select("#artesiaDropDown")
+			.append("select")
+			.attr("id","artesiaDropDownList")
+			.on("change",artesiaChangeSheet)
+			.attr("class","select-css");
+		artesiaDropDown.append("option").attr("value","0").html("Not Selected");
+		// Loop through artesia
+		for(let option of sheets.artesia){
+			addOption(artesiaDropDown,option.title,option);
+		}
+		
+		////////////////////////////////////////////
+		// Artesia
+		let carlsbadDropDown = d3.select("#carlsbadDropDown")
+			.append("select")
+			.attr("id","carlsbadDropDownList")
+			.on("change",carlsbadChangeSheet)
+			.attr("class","select-css");
+		carlsbadDropDown.append("option").attr("value","0").html("Not Selected");
+		// Loop through artesia
+		for(let option of sheets.carlsbad){
+			addOption(carlsbadDropDown,option.title,option);
+		}
+		
+		////////////////////////////////////////////
+		// Artesia
+		let hobbsDropDown = d3.select("#hobbsDropDown")
+			.append("select")
+			.attr("id","hobbsDropDownList")
+			.on("change",hobbsChangeSheet)
+			.attr("class","select-css");
+		hobbsDropDown.append("option").attr("value","0").html("Not Selected");
+		// Loop through artesia
+		for(let option of sheets.hobbs){
+			addOption(hobbsDropDown,option.title,option);
+		}
+		
+		
     });
 
 
@@ -29,14 +63,77 @@ function init(){
     }
 }
 
-function changeSheet(){
-    let value = JSON.parse($("#dropDownList :selected").val());
+function artesiaChangeSheet(){
+	d3.select("#carlsbadDropDownList").property('value', "0");
+	d3.select("#hobbsDropDownList").property('value', "0");
+	d3.select("#errorText").style("display","none");
+	
+    let value = JSON.parse($("#artesiaDropDownList :selected").val());
 
+	if(value == "0"){
+		d3.select("#currentTitle").html("Finance & Insurance");
+		d3.select("#currentSpreadsheet").attr("src",null)
+            .attr("width",parseInt($(window).width())-2)
+            .attr("height",parseInt($(window).height())-28);
+		d3.select("#errorText").style("display",null);
+		return;
+	}
     // Change the title
     d3.select("#currentTitle").html(value.title);
 
     // Change the url
     d3.select("#currentSpreadsheet").attr("src",value.url)
-		.attr("width",parseInt($(window).width())-2)
-		.attr("height",parseInt($(window).height())-28);
+            .attr("width",parseInt($(window).width())-2)
+            .attr("height",parseInt($(window).height())-28);
+	
+	
+}
+
+function carlsbadChangeSheet(){
+	d3.select("#artesiaDropDownList").property('value', "0");
+	d3.select("#hobbsDropDownList").property('value', "0");
+	d3.select("#errorText").style("display","none");
+	
+    let value = JSON.parse($("#carlsbadDropDown :selected").val());
+
+	if(value == "0"){
+		d3.select("#currentTitle").html("Finance & Insurance");
+		d3.select("#currentSpreadsheet").attr("src",null)
+            .attr("width",parseInt($(window).width())-2)
+            .attr("height",parseInt($(window).height())-28);
+		d3.select("#errorText").style("display",null);
+		return;
+	}
+	
+    // Change the title
+    d3.select("#currentTitle").html(value.title);
+
+    // Change the url
+    d3.select("#currentSpreadsheet").attr("src",value.url)
+            .attr("width",parseInt($(window).width())-2)
+            .attr("height",parseInt($(window).height())-28);
+}
+
+function hobbsChangeSheet(){
+	d3.select("#artesiaDropDownList").property('value', "0");
+	d3.select("#carlsbadDropDownList").property('value', "0");
+	d3.select("#errorText").style("display","none");
+	
+    let value = JSON.parse($("#hobbsDropDown :selected").val());
+
+	if(value == "0"){
+		d3.select("#currentTitle").html("Finance & Insurance");
+		d3.select("#currentSpreadsheet").attr("src",null)
+            .attr("width",parseInt($(window).width())-2)
+            .attr("height",parseInt($(window).height())-28);
+		d3.select("#errorText").style("display",null);
+		return;
+	}
+    // Change the title
+    d3.select("#currentTitle").html(value.title);
+
+    // Change the url
+    d3.select("#currentSpreadsheet").attr("src",value.url)
+            .attr("width",parseInt($(window).width())-2)
+            .attr("height",parseInt($(window).height())-28);
 }
